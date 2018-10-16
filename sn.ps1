da40a93b46546789b09fb1d5c9464e5e99873685
John Morgan <johnmorgan302@gmail.com>
2018-10-16 18:48:01 -0500

    Log success vs fail

--- sn.ps1
+++ sn.ps1
@@ -2,12 +2,12 @@ do {
 #   $sn = read-host -Prompt 'Server Number'
     $sn = [System.Console]::ReadKey()
     switch($sn.key){
-        D1 {$myKey = 1}
-        D2 {$myKey = 2}
+        D1 {$myKey = 'Success'}
+        D2 {$myKey = 'Fail'}
         default {$myKey = 0}
     }
     $now = get-date
-    $myStr = 'vm119is01secap'+ $myKey + ' - ' + $now
+    $myStr = $myKey + ' - ' + $now
     if($myKey -gt 0){
         write-Output $myStr >> output.txt
     }
